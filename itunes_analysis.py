0ff63f1b88670b25bc83c6fe8d3a9579e91871c6
Ashley Pak <ashleypak@Ashleys-MacBook-Pro-83.local>
2025-12-10 22:33:46 -0800

    find itunes avg

--- itunes_analysis.py
+++ itunes_analysis.py
@@ -6,4 +6,15 @@ import pandas as pd
 def find_itunes_avg(db_name='music_weather.db'):
     conn = sqlite3.connect(db_name)
     cur = conn.cursor()
-    
+    cur.execute('''
+        SELECT AVG(track_time_millis) / 60000.0 AS avg_track_length_minutes
+        FROM itunes_tracks
+        WHERE track_time_millis IS NOT NULL
+    ''')
+    avg_length = cur.fetchone()[0]
+    cur.execute('''
+        SELECT AVG(track_price) AS avg_track_price
+        FROM itunes_tracks
+        WHERE track_price IS NOT NULL
+    ''')
+    avg_price = cur.fetchone()[0]
